41dde2c12f6e51636de071adc4268e2ddb6a506b
joanjuz <62675444+joanjuz@users.noreply.github.com>
2020-10-01 00:36:28 -0600

    Nose...

---
+++ src/cr.ac.tec.design.prototype/Circles.java
@@ -0,0 +1,17 @@
+package cr.ac.tec.design.prototype;
+
+import java.util.Objects;
+
+public abstract class Circles{
+    public int x;
+    public int y;
+    public int ancho;
+    public int largo;
+}
+public Circles(){
+
+}
+public Circles(Circles target){
+    if (target != )
+
+}
